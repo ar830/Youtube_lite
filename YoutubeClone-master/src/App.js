1d6ad1797a172b37077f7af1d70041024c72464c
ar830 <rajora.aditi24@gmail.com>
2024-06-12 12:49:30 +0530

    First commit

---
+++ YoutubeClone-master/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header.jsx"
+import Feed from "./components/Feed.jsx"
+import LeftNav from "./components/LeftNav.jsx"
+import LeftNavMenuItem from "./components/LeftNavMenuItem.jsx"
+import SearchResult from "./components/SearchResult.jsx"
+import SearchResultVideoCard from "./components/SearchResultVideoCard.jsx"
+import SuggestionVideoCard from "./components/SuggestionVideoCard.jsx"
+import VideoCard from "./components/VideoCard.jsx"
+import VideoDetails from "./components/VideoDetails.jsx"
+
+import { AppContext } from "./context/contextApi";
+
+const App = () => {
+  return (
+    <AppContext>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<Feed/>}/>
+            <Route path="searchResult/:searchQuery" element={<SearchResult/>}/>
+            <Route path="video/:id" element={<VideoDetails/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AppContext>
+  )
+}
+
+export default App;
